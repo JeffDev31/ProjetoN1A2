1703d3d49f8fc48bc6f3be3cdc824ffbfe001a74
JeffDev31 <81516140+JeffDev31@users.noreply.github.com>
2021-10-08 17:34:34 -0300

    Começando o projeto
    Defini o menu e o main.

---
+++ sistemaEventoA2.py
@@ -0,0 +1,18 @@
+def exibirMenu():
+    print("==== Digite 1 para cadastrar novo usuário: ====\n")
+    print("==== Digite 2 para exibir todos os usuários por ordem de cadastro: ====\n")
+    print("==== Digite 3 para exibir todos os usuários por ordem alfabética: ====\n")
+    print("==== Digite 4 para verficar o dado de um usuário pelo seu nome: ====\n")
+    print("==== Digite 5 para verficar o dado de um usuário pelo seu e-mail: ====\n")
+    print("==== Digite 6 para remover um usuário cadastrado: ====\n")
+    print("==== Digite 7 para alterar o nome de um usuário buscando-o por seu e-mail: ====\n")
+    pass
+
+
+def main():
+    exibirMenu()
+    pass
+
+
+if __name__ == "__main__":
+    main()
